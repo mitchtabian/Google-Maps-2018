5a39c0ff99d410bcdbf9ef11a201f1b7d50b16ea
Mitch Tabian <mitch@tabian.ca>
2018-09-12 16:42:03 -0700

    end

--- app/src/main/java/com/codingwithmitch/googlemaps2018/ui/UserListFragment.java
+++ app/src/main/java/com/codingwithmitch/googlemaps2018/ui/UserListFragment.java
@@ -546,12 +546,31 @@ public class UserListFragment extends Fragment implements
                     if(tempDuration < duration){
                         duration = tempDuration;
                         onPolylineClick(polyline);
+                        zoomRoute(polyline.getPoints());
                     }
 
                     mSelectedMarker.setVisible(false);
                 }
             }
         });
+    }
+
+    public void zoomRoute(List<LatLng> lstLatLngRoute) {
+
+        if (mGoogleMap == null || lstLatLngRoute == null || lstLatLngRoute.isEmpty()) return;
+
+        LatLngBounds.Builder boundsBuilder = new LatLngBounds.Builder();
+        for (LatLng latLngPoint : lstLatLngRoute)
+            boundsBuilder.include(latLngPoint);
+
+        int routePadding = 50;
+        LatLngBounds latLngBounds = boundsBuilder.build();
+
+        mGoogleMap.animateCamera(
+                CameraUpdateFactory.newLatLngBounds(latLngBounds, routePadding),
+                600,
+                null
+        );
     }
 
     @Override
