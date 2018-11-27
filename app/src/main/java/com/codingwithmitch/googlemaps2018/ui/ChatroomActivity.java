081124b089d76aaee2cd5184eb99a81f61824a52
lamessk <lamess19@hotmail.com>
2018-11-27 10:57:04 -0700

    -add firebase json

--- app/src/main/java/com/codingwithmitch/googlemaps2018/ui/ChatroomActivity.java
+++ app/src/main/java/com/codingwithmitch/googlemaps2018/ui/ChatroomActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 import com.codingwithmitch.googlemaps2018.R;
